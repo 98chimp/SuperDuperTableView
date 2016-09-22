39ace303a627056809956f62bac3a283f8c21b6f
Shahin Zangenehpour <shahin@98chimp.com>
2016-09-22 16:32:34 -0700

    Initial Commit

---
+++ SuperDuperTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperDuperTableView
+//
+//  Created by Shahin on 2016-09-22.
+//  Copyright © 2016 98%Chimp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuperDuperTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuperDuperTableView
+//
+//  Created by Shahin on 2016-09-22.
+//  Copyright © 2016 98%Chimp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
